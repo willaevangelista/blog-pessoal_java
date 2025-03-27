c2b90a1a65ae6b5a622de2e0a4accd8a2fde7124
Willa Evangelista <willaevangelista.pro@gmail.com>
2025-03-27 15:32:59 -0300

    Inclui as anotações @EnableJpaRepositories e @EntityScan na classe BlogpessoalApplication para garantir que o Spring encontre corretamente os repositórios e entidades do projeto.

--- src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -2,8 +2,14 @@ package com.generation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.generation.blogpessoal.repository") 
+@EntityScan("com.generation.blogpessoal.model")  
+@ComponentScan("com.generation.blogpessoal")
 public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
